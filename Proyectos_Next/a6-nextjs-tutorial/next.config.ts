f600dbe43bbaf90ca38beafa067777fe2f1e6989
Andreu-08 <andreuorenga@gmail.com>
2024-12-16 15:23:52 +0100

    add typescript

--- Proyectos_Next/a6-nextjs-tutorial/next.config.ts
+++ Proyectos_Next/a6-nextjs-tutorial/next.config.ts
@@ -3,8 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: { 
     ignoreDuringBuilds: true
-  }
+  },
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
 
 export default nextConfig;
