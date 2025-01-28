54bf16af04136760c2c163c8f89630ba9b499cc9
Andreu Orenga <andreuorenga@gmail.com>
2025-01-28 15:42:53 +0000

    SEED

--- Next/social-app/social-app/app/seed/page.jsx
+++ Next/social-app/social-app/app/seed/page.jsx
@@ -3,11 +3,11 @@ import {sql} from '@vercel/postgres'
 
 export default async () => {
     //consulta que ejecutara al navegar a /seed
-    await sql`CREATE TABLE IF NOT EXIST POST(
+    /*await sql`CREATE TABLE IF NOT EXIST POST(
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         content TEXT,
         url TEXT   
-    )`
+    )`*/
 
     //mensaje que devuelve la ruta
     return <p>Database seed the guay</p>
